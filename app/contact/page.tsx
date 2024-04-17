98e864c5adddc04fca82f65936907078520572dd
cezarszl <cezarszlfedora@gmail.com>
2024-04-17 13:18:47 +0200

    Updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,6 +14,14 @@ const Contact = () => {
       <div className="flex items-center justify-center">
         <ContactForm />
       </div>
+      <p className="mt-10 text-xl font-normal">
+        Open to remote, freelance, and full-time opportunities in software
+        development.
+        <br />
+        Please feel free to reach out to me with any opportunities or inquiries.
+        <br />
+        Thank you for considering my portfolio.
+      </p>
     </main>
   );
 };
