9be48f659746093f01fc804f4b74b1a0e37df398
cezarszl <cezarszlfedora@gmail.com>
2024-04-17 13:16:45 +0200

    Updated aboutme

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,16 +32,14 @@ const About = () => {
         </div>
         <div className="w-full md:w-6/12">
           <p className="text-xl font-normal text-justify">
-            As a Certified Full-Stack Web Developer, I bring a diverse skill set
-            honed through experience in HR and recruitment, as well as rigorous
-            technical training. With a track record of facilitating over 100
-            successful placements at a work agency and implementing quality
-            control procedures that resulted in a 20% increase in product
-            reliability in a food manufacture, I excel in client relationship
-            management, problem-solving, and collaboration. My proficiency in
-            HTML, CSS, JavaScript, React, Node.js, and database management
-            equips me to tackle complex web development projects with precision
-            and efficiency.
+            Born and raised in Częstochowa, Poland. I fell in love with
+            computers and technology from the first Commodore 64. For this
+            reason, I educated myself at the local university in the department
+            of computer science. After a long 10-year stay in Great Britain and
+            gaining experience in other industries, I joined the rest of my
+            family and moved to Hanover, Germany. With major changes, I decided
+            to take part in CareersFoundry's bootcamp and return to what I love,
+            which is programming.
           </p>
           <div className="mt-[3rem] flex flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center justify-center sm:space-x-6">
             <a
@@ -58,13 +56,36 @@ const About = () => {
         </div>
         <div className="hidden md:block md:w-6/12">
           <Image
-            src="/images/about.jpeg"
+            src="/images/about.png"
             width={1200}
             height={1600}
             alt="Cezary's personal headshot"
             className="overflow-hidden w-full object-cover border-2 border-gray-300 filter grayscale transition-all duration-500 ease-in-out cursor-cell hover:grayscale-0 hover:scale-102"
           />
         </div>
+      </div>
+      <div>
+        <p className="text-xl font-normal text-justify mt-20">
+          Besides new technologies, I am a big fan of football, tennis, and
+          esports. Moreover, I try to spend every vacation traveling. In 2023, I
+          completed the Caminho de Portugal, which is over 300 km of hiking
+          through Portugal to Santiago de Compostela in Spain.
+        </p>
+        <p className="text-xl font-normal text-justify mt-12">
+          I enjoy the full spectrum of web development, but I'm particularly
+          passionate about integrating front-end and back-end technologies to
+          create seamless user experiences. On the front-end, I love crafting
+          interactive and visually appealing interfaces using modern frameworks
+          like React or Angular. This involves a lot of creative problem-solving
+          and design thinking, which are aspects I find deeply rewarding. On the
+          back-end, I'm intrigued by the challenge of designing robust APIs and
+          optimizing database interactions to ensure performance and
+          scalability. Working with databases, whether SQL or NoSQL, allows me
+          to structure data effectively and leverage it to drive the
+          application's functionality. What excites me the most is seeing all
+          these components come together into a cohesive and functional
+          application that provides real value to users.
+        </p>
       </div>
     </main>
   );
