6b0c45949ce25116ace885e0e28e82d7433ce112
cezarszl <cezarszlfedora@gmail.com>
2024-04-17 13:17:41 +0200

    Added impressum

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,6 +7,7 @@ const Footer = () => {
       <hr className="h-px w-4/5 md:w-3/5 mx-auto my-0" />
       <div className="flex md:w-3/5 mx-auto justify-around items-center gap-y-0 gap-x-4 py-8">
         <p>© {new Date().getFullYear()} Cezary Szal&apos;s Portfolio</p>
+        <Link href="/impressum">Impressum</Link>
         <div className="flex gap-5 md:gap-10">
           {FooterLinks.map((footerLink) => (
             <Link key={footerLink.name} href={footerLink.link}>
