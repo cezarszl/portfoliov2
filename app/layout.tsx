0f102f87601592f0e7056e362c9f6f9e14554e0c
Cezary Szal <146932043+cezarszl@users.noreply.github.com>
2024-05-25 22:58:12 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Metadata } from "next";
 import i18nConfig from "@/app/i18nConfig";
 
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang={locale}>
       <head>
-       <script dangerouslySetInnerHTML={{ __html: `
+        <script dangerouslySetInnerHTML={{ __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -28,15 +29,13 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-5MP9792D');
         ` }} />
       </head>
-  
       <body>
-        
-             <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MP9792D"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        
-        
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MP9792D"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
+          </iframe>
+        </noscript>
         {children}
-   
       </body>
     </html>
   );
