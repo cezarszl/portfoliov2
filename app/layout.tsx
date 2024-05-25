44edc641cfd07e8c96cf7979a327e971b6d23a46
Cezary Szal <146932043+cezarszl@users.noreply.github.com>
2024-05-25 23:19:03 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
- import type { Metadata } from 'next';
+import type { Metadata } from 'next';
 import i18nConfig from '@/app/i18nConfig';
 import { ReactNode } from 'react';
-import { Html, Head} from 'next/document';
+
 
 export const metadata: Metadata = {
   title: 'Cezary Szal - Web Developer',
@@ -20,24 +20,11 @@ export default function RootLayout({
   params: { locale: string };
 }) {
   return (
-    <Html lang={locale}>
-      <Head>
-        <script dangerouslySetInnerHTML={{ __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5MP9792D');
-        ` }} />
-      </Head>
+   <html lang={locale}>
+      <head />
       <body>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MP9792D"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
-          </iframe>
-        </noscript>
         {children}
       </body>
-    </Html>
+    </html>
   );
 }
