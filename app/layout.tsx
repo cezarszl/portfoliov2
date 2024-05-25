3e9005151571196bddd107f8b4fe4f5c9bc04558
cezarszl <cezarszlfedora@gmail.com>
2024-05-26 00:11:52 +0200

    Added google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from 'next';
 import i18nConfig from '@/app/i18nConfig';
 import { ReactNode } from 'react';
+import { GoogleTagManager } from '@next/third-parties/google' 
 
 
 export const metadata: Metadata = {
   title: 'Cezary Szal - Web Developer',
   description: 'Cezary Szal - Web Developer',
 };
-
 export function generateStaticParams() {
   return i18nConfig.locales.map((locale) => ({ locale }));
 }
@@ -21,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
    <html lang={locale}>
+     <GoogleTagManager gtmId="GTM-5MP9792D" />
       <body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MP9792D" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         {children}
       </body>
     </html>
