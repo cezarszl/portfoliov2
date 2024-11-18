25aa52c15ca5ee282c79061b9564cfd4153540d4
cezarszl <cezarszlfedora@gmail.com>
2024-11-18 22:15:23 +0100

    Fix layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Dotyczy wszystkich stron w aplikacji
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=0, must-revalidate",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
-
-
